c4fd21d34bbec7fe1c1457638afad9586c1e7aa5
Hamdi Taha <hamdi.taha@esprit.tn>
2024-02-23 16:47:09 +0100

    provider fixed

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,35 +1,32 @@
-"use client"
-import { QueryClient } from "@tanstack/react-query"
-import { httpBatchLink } from "@trpc/client"
-import { PropsWithChildren, ReactNode, useState } from "react"
-import { trpc } from "@/app/_trpc/client"
-import { Provider as TrpcProvider } from "@trpc/react"
-import { createClient } from "@trpc/client";
+'use client'
 
+import { trpc } from '@/app/_trpc/client'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { httpBatchLink } from '@trpc/client'
+import { PropsWithChildren, useState } from 'react'
+import { createClient } from '@trpc/client';
 
-const Providers = ({children}: {children: ReactNode}) => {
-    
-    const [queryClient] = useState(() => new QueryClient())
-    const Providers = ({children}: {children: ReactNode}) => {
+const Providers = ({ children }: PropsWithChildren) => {
 
-        const [trpcClient] = useState(() =>
-            createClient({
-                links: [
-                    httpBatchLink({
-                        url: "https://localhost:3000/api/trpc",
-                    }),
-                ],
-            })
-        );
-        return(
-            <TrpcProvider client={trpcClient} queryClient={queryClient}>
-                {children}
-            </TrpcProvider>
-        )
-    }
-        
-    
-    
+const [queryClient] = useState(() => new QueryClient());
+const [trpcClient] = useState(() =>
+    createClient({
+        links: [
+            httpBatchLink({
+                url: ("https/localhost:3000/api/trpc"),
+            }),
+        ],
+    })
+);
+  return (
+      
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+  )
 }
 
 export default Providers
